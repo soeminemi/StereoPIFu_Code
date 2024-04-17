78c3ced2e1f87c0fc4406535ae2e55a549c91fd0
soeminemi <jnwangcsc@gmail.com>
2024-04-17 22:03:05 +0800

    Update eval.sh

--- eval.sh
+++ eval.sh
@@ -4,8 +4,8 @@ python eval.py \
 --test_data_dir "./TempData/TexMesh" \
 --gpu_idx 0
 
-python eval.py \
---model_path "./Models/StereoPIFu_model.pth" \
---save_dir "./Results" \
---test_data_dir "./TempData/ColorMesh" \
---gpu_idx 0
+# python eval.py \
+# --model_path "./Models/StereoPIFu_model.pth" \
+# --save_dir "./Results" \
+# --test_data_dir "./TempData/ColorMesh" \
+# --gpu_idx 0
